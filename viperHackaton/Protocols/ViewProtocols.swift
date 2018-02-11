5675a6d81902858fd4fa9b60f0dd59dfd9ea9d66
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2018-02-11 13:49:46 -0300

    Add view protocols

--- viperHackaton/Protocols/ViewProtocols.swift
+++ viperHackaton/Protocols/ViewProtocols.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+protocol ReusableView: class {}
+
+protocol NibLoadableView: class { }
+
+protocol IndicatableView: class {
+	func showActivityIndicator()
+	func hideActivityIndicator()
+}
